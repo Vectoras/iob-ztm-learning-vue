58ad14a7afc6cd8d5a3d54193e2db69ae1158a2e
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-02-17 16:07:22 +0000

    3-34 -> project updated to the trainer solution

--- Section 3/app.js
+++ Section 3/app.js
@@ -1,54 +1,25 @@
 const vm = Vue.createApp({
   data() {
     return {
-      perspective_v: 100,
-      rotateX_v: 0,
-      rotateY_v: 0,
-      rotateZ_v: 0,
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
     };
   },
   computed: {
-    perspective() {
-      return `${this.perspective_v}px`;
-    },
-    rotateX() {
-      return `${this.rotateX_v}deg`;
-    },
-    rotateY() {
-      return `${this.rotateY_v}deg`;
-    },
-    rotateZ() {
-      return `${this.rotateZ_v}deg`;
-    },
     box_inlineStyling() {
       return {
-        transform: `rotateX(${this.rotateX}) rotateY(${this.rotateY}) rotateZ(${this.rotateZ})`,
-      };
-    },
-    container_inlineStyling() {
-      return {
-        perspective: this.perspective,
+        transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`,
       };
     },
   },
   methods: {
-    updatePerspective(event) {
-      this.perspective_v = event.target.valueAsNumber;
-    },
-    updateRotateX(event) {
-      this.rotateX_v = event.target.valueAsNumber;
-    },
-    updateRotateY(event) {
-      this.rotateY_v = event.target.valueAsNumber;
-    },
-    updateRotateZ(event) {
-      this.rotateZ_v = event.target.valueAsNumber;
-    },
     resetAllValues() {
-      this.perspective_v = 100;
-      this.rotateX_v = 0;
-      this.rotateY_v = 0;
-      this.rotateZ_v = 0;
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
     },
   },
   watch: {},
