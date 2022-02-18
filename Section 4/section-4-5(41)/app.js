1159158627b6aff1941808421c6cce1ebe83dcdb
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-02-18 14:54:23 +0000

    4-44

--- Section 4/section-4-5(41)/app.js
+++ Section 4/section-4-5(41)/app.js
@@ -1,11 +1,13 @@
-let vm = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!",
-    };
-  },
-  // template: `{{message}}`,
-});
+// let vm = Vue.createApp({
+//   data() {
+//   return {
+//     message: "Hello world!",
+//   };
+// },
+// template: `{{message}}`,
+// });
+
+const vm = Vue.createApp({});
 
 vm.component("hello", {
   template: `<h1>{{ message }}</h1>`,
