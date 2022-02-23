e1f2799cf5002bc52019c329175b37388b158d06
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-02-23 09:34:15 +0000

    7 - 76 -> CSS animations

---
+++ Section 7/section-7-1[74]/animations/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
+
+  <!-- <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello World!</h2>
+    <h2 v-else key="secondary">Bye bye!</h2>
+  </transition> -->
+
+  <transition name="zoom" appear type="">
+    <h2 v-if="flag" key="main">Hello World!</h2>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    components: {},
+    data() {
+      return {
+        flag: true,
+      };
+    },
+  };
+</script>
+
+<style>
+  h2 {
+    width: 400px;
+    padding: 20px;
+    margin: 20px;
+  }
+
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: all 0.25s linear;
+  }
+  .fade-leave-to {
+    transition: all 0.25s linear;
+    opacity: 0;
+  }
+
+  .zoom-enter-from {
+    opacity: 0;
+  }
+  .zoom-leave-to {
+    opacity: 0;
+  }
+
+  .zoom-enter-active {
+    animation: zoom-in 1s linear forwards;
+    transition: all 2s linear;
+  }
+  .zoom-leave-active {
+    animation: zoom-out 1s linear forwards;
+    transition: all 2s linear;
+  }
+
+  @keyframes zoom-in {
+    from {
+      transform: scale(0, 0);
+    }
+    to {
+      transform: scale(1, 1);
+    }
+  }
+  @keyframes zoom-out {
+    from {
+      transform: scale(1, 1);
+    }
+    to {
+      transform: scale(0, 0);
+    }
+  }
+</style>
