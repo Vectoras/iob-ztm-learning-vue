cdceb5f8a5ed5d9f4c7ac189a17b78b3a180e694
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-03-04 19:36:39 +0000

    10 - 122 -> slots mini project

---
+++ Section 10/section-10-12/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <hello-world #default="{user, fav}">
+      <p>Hello {{ user.name }}</p>
+      <p>Your favorites food are</p>
+      <ul>
+        <li v-for="f in fav" :key="f">{{ f }}</li>
+      </ul>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from "./components/HelloWorld.vue";
+
+  export default {
+    name: "App",
+    components: {
+      HelloWorld,
+    },
+  };
+</script>
