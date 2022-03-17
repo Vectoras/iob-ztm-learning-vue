c5bd3c081716fb26bfca83a4e4b708b9ff33c403
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-03-17 23:48:12 +0000

    21 - 277

---
+++ Section 21/section-21-11-275/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    showAbout: false
+  },
+  getters: {
+    showAbout(state) {
+      return state.showAbout;
+    }
+  },
+  mutations: {
+    setShowAbout(state, payload) {
+      state.showAbout = payload;
+    }
+  },
+  actions: {},
+  modules: {}
+});
