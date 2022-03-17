2ef9fa677ee9ef82a5936016b3f4e1d4daa5b845
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-03-17 22:46:33 +0000

    21 - 273

--- Section 21/section-21-3-267/src/App.vue
+++ Section 21/section-21-3-267/src/App.vue
@@ -1,9 +1,88 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ num }}</p>
+    <p>{{ double }}</p>
+    <button type="button" @click.prevent="increment">Click me</button>
+
+    <p>{{ name }}</p>
+
+    <input type="text" v-model="phrase" />
+    <p>{{ reversedPhrase }}</p>
+
+    <button type="button" ref="btn">Button</button>
+  </div>
+
+  <app-alert :user="user" />
 </template>
 
 <script>
-export default {
-  name: "App",
-};
+  import { ref, reactive, toRefs, watch, computed, onBeforeMount, onMounted } from "vue";
+  import AppAlert from "@/components/Alert.vue";
+
+  export default {
+    name: "App",
+    components: {
+      AppAlert,
+    },
+    setup() {
+      const btn = ref(null);
+
+      onBeforeMount(() => {
+        console.log("onBeforeMount()");
+      });
+      onMounted(() => {
+        console.log("onMounted()");
+
+        btn.value.addEventListener("click", () => {
+          console.log("button clicked");
+        });
+      });
+
+      let num = ref(0);
+      function increment() {
+        num.value++;
+      }
+
+      const double = computed(() => {
+        return num.value * 2;
+      });
+
+      const user = reactive({
+        name: "John",
+        age: 20,
+      });
+
+      setTimeout(() => {
+        user.name = "Luis";
+      }, 3000);
+
+      const phrase = ref("");
+      const reversedPhrase = ref("");
+
+      // watch([phrase], ([newVal], [oldVal]) => {
+      //   reversedPhrase.value = phrase.value
+      //     .split("")
+      //     .reverse()
+      //     .join("");
+      // });
+
+      watch(() => {
+        reversedPhrase.value = phrase.value
+          .split("")
+          .reverse()
+          .join("");
+      });
+
+      return {
+        num,
+        increment,
+        ...toRefs(user),
+        phrase,
+        reversedPhrase,
+        double,
+        user,
+        btn,
+      };
+    },
+  };
 </script>
---
+++ Section 21/section-21-3-267/src/components/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="!flag" style="color:red;">Name changed!</div>
+</template>
+
+<script>
+  import { computed } from "vue";
+
+  export default {
+    name: "AppAlert",
+    props: ["user"],
+
+    setup(props, context) {
+      // context.emit("eventName", {});
+
+      const flag = computed(() => {
+        return props.user.name === "John";
+      });
+
+      return {
+        flag,
+      };
+    },
+  };
+</script>
